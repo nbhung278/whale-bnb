4244206a49147cce0c18608fd486c21caac24234
nbhung278 <nbhung278@gmail.com>
2023-07-12 02:09:32 +0700

    add autoplay to content image

--- client/src/common/items/BaseItem.tsx
+++ client/src/common/items/BaseItem.tsx
@@ -3,7 +3,7 @@ import { FiStar } from "react-icons/fi";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper";
+import { Pagination, Autoplay } from "swiper";
 import "./style.css";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
@@ -14,10 +14,15 @@ const BaseItem = ({ data }: any) => {
 		<div>
 			<div className="w-[320px] h-[300px] rounded-xl overflow-hidden relative">
 				<Swiper
+					loop={true}
+					// autoplay={{
+					// 	delay: 10000,
+					// 	disableOnInteraction: false,
+					// }}
 					pagination={{
 						dynamicBullets: true,
 					}}
-					modules={[Pagination]}
+					modules={[Pagination, Autoplay]}
 					className="mySwiper cursor-pointer"
 				>
 					{data.images?.map((image: any) => {
@@ -34,9 +39,8 @@ const BaseItem = ({ data }: any) => {
 					})}
 				</Swiper>
 				<div className="absolute z-10 top-3 right-3 cursor-pointer">
-					<AiOutlineHeart size='24px' color='white' />
+					<AiOutlineHeart size="24px" color="white" />
 					{/* <AiFillHeart size='24px' color='white' /> */}
-					
 				</div>
 			</div>
 			<div className="grid mt-2 grid-cols-5 justify-between font-bold gap-4">
